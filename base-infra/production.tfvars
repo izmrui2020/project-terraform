ad60bdc55ec2a62b13bffb310f868260e1c4c11c
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-06-29 02:59:53 +0900

    finish base infra

---
+++ base-infra/production.tfvars
@@ -0,0 +1,8 @@
+# VPC variables for production
+vpc_cidr = "10.0.0.0/16"
+
+public_subnet_1a_cidr = "10.0.1.0/24"
+public_subnet_1c_cidr = "10.0.2.0/24"
+
+private_subnet_2a_cidr = "10.0.3.0/24"
+private_subnet_2a_cidr = "10.0.4.0/24"
--- base-infra/vpc.tf
+++ base-infra/vpc.tf
@@ -89,10 +89,58 @@ resource "aws_route_table_association" "private-route-2c-association" {
   subnet_id      = aws_subnet.private-subnet-2c.id
 }
 
-resource "aws_eip" "elastic-ip-for-nat-gw" {
+resource "aws_eip" "elastic-ip-for-nat-gw-1a" {
   vpc                       = true
   associate_with_private_ip = "10.0.0.5"
   tags {
-    Name = "Production-EIP"
+    Name = "Production-EIP-1a"
   }
+}
+
+# resource "aws_eip" "elastic-ip-for-nat-gw-1c" {
+#   vpc                       = true
+#   associate_with_private_ip = "10.0.0.6"
+#   tags {
+#     Name = "Production-EIP-1c"
+#   }
+# }
+
+resource "aws_nat_gateway" "nat-gw-1a" {
+  allocation_id = aws_eip.elastic-ip-for-nat-gw-1a.id
+  subnet_id     = aws_subnet.public-subnet-1a.id
+
+  tags {
+    Name = "Production-NAT-GW-1a"
+  }
+  depends_on = ["aws_eip.elastic-ip-for-nat-gw-1a"]
+}
+
+# resource "aws_nat_gateway" "nat-gw-1c" {
+#   allocation_id = "${aws_eip.elastic-ip-for-nat-gw-1c.id}"
+#   subnet_id     = "${aws_subnet.public-subnet-1c.id}"
+#
+#   tags {
+#     Name = "Production-NAT-GW-1c"
+#   }
+#
+#   depends_on    = ["aws_eip.elastic-ip-for-nat-gw-1c"]
+# }
+
+resource "aws_route" "nat-gw-route" {
+  route_table_id        = aws_route_table.private-route-table.id
+  nat_gateway_id        = aws_nat_gateway.nat-gw-1a.id
+  destination_cidrblock = "0.0.0.0/0"
+}
+
+resource "aws_internet_gateway" "production-igw" {
+  vpc_id = aws_vpc.production-vpc.id
+  tags {
+    Name = "Production-IGW"
+  }
+}
+
+resource "aws_route" "public-internet-gw-route" {
+  route_table_id        = aws_route_table.public-route-table.id
+  gateway_id            = aws_internet_gateway.production-igw.id
+  destination_cidrblock = "0.0.0.0/0"
 }
