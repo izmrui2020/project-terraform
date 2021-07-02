3ab014625831be855213749eab501ce7453e0143
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-07-03 02:24:34 +0900

    ファイル変更

---
+++ 3-application/app.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = var.region
+}
+
+terraform {
+  backend "s3" {}
+}
+
+data "terraform_remote_state" "platform" {
+  backend = "s3"
+
+  config = {
+    key     = var.remote_state_key
+    bucket  = var.remote_state_bucket
+    region  = var.region
+  }
+}
+
